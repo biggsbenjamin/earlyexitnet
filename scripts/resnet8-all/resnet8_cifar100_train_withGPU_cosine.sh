6063374939e589f3225eb25667a4fc47a29389a8
Ben Biggs <bb2515@ee-tiamat.ee.ic.ac.uk>
2025-04-14 17:07:43 +0100

    WIP resnet8 on cifar100 - test script and prelim model

--- scripts/resnet8-all/resnet8_cifar100_train_withGPU_cosine.sh
+++ scripts/resnet8-all/resnet8_cifar100_train_withGPU_cosine.sh
@@ -2,4 +2,4 @@
 
 # Training resnet8 (not necessarily well) but very quickly on a chonky gpu
 
-python -m earlyexitnet.cli -m resnet8_bb -d cifar100 -bstr 32 -bbe 2 -bbo 'adam-wd-cos-sched' -vf 11 -gpu 1 -nw 16 -bste 1000 -rn "cifar100 dataset, aiming for ~70 t1"
+python -m earlyexitnet.cli -m resnet8_bb -d cifar100 -bstr 32 -bbe 2000 -bbo 'adam-wd-cos-sched' -vf 11 -gpu 1 -nw 64 -bste 1000 -rn "cifar100 dataset, aiming for ~70 t1"
